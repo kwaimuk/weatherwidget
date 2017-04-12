8c568c6e4b855c3490e513519c02ca57bb87f34b
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-04-11 14:11:00 -1000

    able to display temp

--- public/js/client.js
+++ public/js/client.js
@@ -15,10 +15,31 @@ function inquiry() {
 
    const city = requestData.city.name;
    const country = requestData.city.country;
+   const date = requestData.list["0"].dt_txt;
+   const date2 = requestData.list["8"].dt_txt;
+   const date3 = requestData.list["16"].dt_txt;
+   const date4 = requestData.list["24"].dt_txt;
+   const date5 = requestData.list["32"].dt_txt;
+
+   const main = requestData.list["0"].main.temp;
+   console.log(main);
+   const weather = requestData.list["0"].weather;
+
+/*   for(let i= 0, i<4,i++){
+
+   }*/
 
 
    document.querySelector("#city").innerHTML = `City: ${city}`;
    document.querySelector("#country").innerHTML = `Country: ${country}`;
+   document.querySelector("#one").innerHTML = `Date: ${date}`;
+   // document.querySelector("#one").innerHTML = `Date: ${main}`;
+   // document.querySelector("#one").innerHTML = `Date: ${weather}`;
+
+   document.querySelector("#two").innerHTML = `Date: ${date2}`;
+   document.querySelector("#three").innerHTML = `Date: ${date3}`;
+   document.querySelector("#four").innerHTML = `Date: ${date4}`;
+   document.querySelector("#five").innerHTML = `Date: ${date5}`;
 
 
 };
